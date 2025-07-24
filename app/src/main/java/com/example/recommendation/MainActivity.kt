d09caa7cea5aaa6f039996c1da5123d32603a932
s_ong_e <yunsezero0102@gmail.com>
2025-07-24 10:16:09 +0900

    Initial commit

--- app/src/main/java/com/example/recommendation/MainActivity.kt
+++ app/src/main/java/com/example/recommendation/MainActivity.kt
@@ -1,20 +1,19 @@
 package com.example.recommendation
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(R.layout.activity_main) // 이건 main의 XML 이름
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            val intent = Intent(this, RecordActivity::class.java)
+            startActivity(intent)
         }
     }
 }
---
+++ app/src/main/java/com/example/recommendation/RecordActivity.kt
@@ -0,0 +1,4 @@
+package com.example.recommendation
+
+class RecordActivity {
+}
